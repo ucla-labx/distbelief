17fbf4b6b5d340aed70f45179048ff508a982827
Jesse Cai <jcjessecai@gmail.com>
2018-08-03 09:52:09 -0700

    added more analysis

--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ install:
 	pip install .
 
 single:
-	python example/main.py --no-distributed --epochs 2
+	python example/main.py --no-distributed --cuda
--- example/main.py
+++ example/main.py
@@ -19,7 +19,6 @@ import torch.optim as optim
 from distbelief.optim import DownpourSGD
 from distbelief.server import ParameterServer
 
-
 def get_dataset(args, transform):
     """
     :param dataset_name:
@@ -147,8 +146,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Distbelief training example')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=10000, metavar='N', help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: 0.1)')
+    parser.add_argument('--epochs', type=int, default=20, metavar='N', help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR', help='learning rate (default: 0.1)')
     parser.add_argument('--freq', type=int, default=10, metavar='N', help='how often to send/pull grads (default: 10)')
     parser.add_argument('--cuda', action='store_true', default=False, help='use CUDA for training')
     parser.add_argument('--log-interval', type=int, default=20, metavar='N', help='how often to evaluate and print out')

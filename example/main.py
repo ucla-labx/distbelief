03e19aec31eeba510bde387cf5ef5b6b3415932f
Jesse Cai <jesse@blend.com>
2018-08-04 13:06:03 -0700

    updated single .csv

--- example/main.py
+++ example/main.py
@@ -104,7 +104,7 @@ def main(args):
     df = pd.DataFrame(logs)
     print(df)
     if args.no_distributed:
-        df.to_csv('single.csv')
+        df.to_csv('single.csv', index_label='index')
     else:
         df.to_csv('node{}.csv'.format(dist.get_rank()))
     print('Finished Training')
@@ -147,7 +147,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=10000, metavar='N', help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=20, metavar='N', help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR', help='learning rate (default: 0.1)')
+    parser.add_argument('--lr', type=float, default=0.1, metavar='LR', help='learning rate (default: 0.1)')
     parser.add_argument('--freq', type=int, default=10, metavar='N', help='how often to send/pull grads (default: 10)')
     parser.add_argument('--cuda', action='store_true', default=False, help='use CUDA for training')
     parser.add_argument('--log-interval', type=int, default=20, metavar='N', help='how often to evaluate and print out')
